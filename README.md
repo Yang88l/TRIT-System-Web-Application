7c9f59533d5bde68006308dbe8396b06d03b15f7
Yang88l <hongyanglim00@gmail.com>
2025-04-01 18:15:47 +0800

    added css with bootstrap

--- README.md
+++ README.md
@@ -1,2 +1,10 @@
 # TRIT-System-Web-Application
 A system that manage the activities of a computer repair shop.
+
+##To run flask:
+
+Step 1: Set environment variable
+$env:FLASK_APP = "application.py"
+
+Step 2: Run the flask app
+flask run
--- application.py
+++ application.py
@@ -1,5 +1,17 @@
-from flask import Flask
+from flask import Flask, render_template, request
 
 app = Flask(__name__)
 
-print(__name__)
+@app.route('/', methods=['GET', 'POST'])
+def index():
+    name = request.form.get('name')
+    headline="Welcome to the TRIT System Web Application"
+    return render_template('index.html',  headline=headline)
+
+@app.route('/about')
+def about():
+    return "This is the TRIT System Web Application. It helps in managing and analyzing data."
+
+@app.route('/<string:name>')
+def get_name(name):
+    return f"<h1>Hello, {name}!</h1>"
